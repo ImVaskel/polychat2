c42e0c6369f77e137d04f8a9c921c5a3e5b92911
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2023-01-10 19:49:54 -0500

    i question you john

--- client/forge-1.19/src/main/java/club/moddedminecraft/polychat2/Polychat2.java
+++ client/forge-1.19/src/main/java/club/moddedminecraft/polychat2/Polychat2.java
@@ -5,25 +5,23 @@ import club.moddedminecraft.polychat.client.clientbase.CommandRunner;
 import club.moddedminecraft.polychat.client.clientbase.PolychatClient;
 import club.moddedminecraft.polychat.core.messagelibrary.ServerProtos;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.slf4j.Logger;
+
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,7 +29,6 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Polychat2.MODID)
 public class Polychat2 implements ClientApiBase
 {
@@ -39,10 +36,6 @@ public class Polychat2 implements ClientApiBase
     private MinecraftServer server;
     // Define mod id in a common place for everything to reference
     public static final String MODID = "polychat2";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-
 
     public Polychat2()
     {
--- client/forge-1.19/src/main/java/club/moddedminecraft/polychat2/Runner.java
+++ client/forge-1.19/src/main/java/club/moddedminecraft/polychat2/Runner.java
@@ -1,14 +1,12 @@
 package club.moddedminecraft.polychat2;
 
 import club.moddedminecraft.polychat.client.clientbase.CommandRunner;
-import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.storage.ServerLevelData;
 import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
 
